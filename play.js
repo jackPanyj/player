986b32f94e52e03173cbcfc77eebe1494883adb8
jackPanyj <1016432619@qq.com>
2016-01-22 23:03:55 +0800

    music change

--- play.js
+++ play.js
@@ -1,12 +1,16 @@
 var audio = document.getElementsByTagName('audio')[0],
  controls = document.getElementsByTagName('rowspan'),
- pic = document.images[0];
+ pic = document.images[0],
+ spans = document.getElementsByTagName('span');
 controls[0].addEventListener('click', play);
 controls[1].addEventListener('click', stop);
+spans[0].addEventListener('click', show_1);
+spans[1].addEventListener('click', show_2);
 controls[0].click();
 var timer = null;
 function play(){
 	audio.play();
+	clearInterval(timer);
 	timer = setInterval(function() {
 		var width = window.innerWidth > 1200 ? 68: 78;
 		var song_time = audio.duration;
@@ -19,5 +23,12 @@ function stop(){
 	audio.pause();
 }
 
-	
+function show_1(){
+	audio.src = "http://m10.music.126.net/20160122232648/8f6e73677e7c71f57581d5377ee7e0c6/ymusic/1740/3891/913f/33532b549afe2ada4caf071d9ade35f4.mp3";
+}	controls[0].click();
 
+function show_2(){
+	audio.src='http://m10.music.126.net/20160122230600/04c05c27900dcf221ab791d9fecd6606/ymusic/1740/3891/913f/33532b549afe2ada4caf071d9ade35f4.mp3'
+"http://m10.music.126.net/20160122230600/04c05c27900dcf221ab791d9fecd6606/ymusic/1740/3891/913f/33532b549afe2ada4caf071d9ade35f4.mp3";
+	controls[0].click();
+}
