b9f401da77ce3da461b358ee9283e63a2a78a882
jackPanyj <1016432619@qq.com>
2016-01-22 23:42:09 +0800

    fix some bug

--- play.js
+++ play.js
@@ -11,10 +11,11 @@ var timer = null;
 function play(){
 	audio.play();
 	clearInterval(timer);
+	var play_border,song_time;
 	timer = setInterval(function() {
-		var width = window.innerWidth > 1200 ? 68: 78;
-		var song_time = audio.duration;
-		pic.style.left = audio.currentTime/song_time * width + 'vw';
+		play_border = document.querySelector('.play_border'),
+			  song_time = audio.duration;
+		pic.style.left = audio.currentTime/song_time * play_border.getBoundingClientRect().width + 'px';
 	}, 200);
 }
 
@@ -24,11 +25,10 @@ function stop(){
 }
 
 function show_1(){
-	audio.src = "http://m10.music.126.net/20160122232648/8f6e73677e7c71f57581d5377ee7e0c6/ymusic/1740/3891/913f/33532b549afe2ada4caf071d9ade35f4.mp3";
+	audio.src = "http://m10.music.126.net/20170122232648/8f6e73677e7c71f57581d5377ee7e0c6/ymusic/1740/3891/913f/33532b549afe2ada4caf071d9ade35f4.mp3";
 }	controls[0].click();
 
 function show_2(){
-	audio.src='http://m10.music.126.net/20160122230600/04c05c27900dcf221ab791d9fecd6606/ymusic/1740/3891/913f/33532b549afe2ada4caf071d9ade35f4.mp3'
-"http://m10.music.126.net/20160122230600/04c05c27900dcf221ab791d9fecd6606/ymusic/1740/3891/913f/33532b549afe2ada4caf071d9ade35f4.mp3";
+	audio.src='http://m10.music.126.net/20160122232648/8f6e73677e7c71f57581d5377ee7e0c6/ymusic/1740/3891/913f/33532b549afe2ada4caf071d9ade35f4.mp3';
 	controls[0].click();
 }
